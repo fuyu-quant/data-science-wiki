2e9c74f59476ba9b912de8a417de3410d75ebc2b
toma tanaka <ulti4929@gmail.com>
2023-07-15 23:59:07 +0900

    update

--- utils/git_config.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name fuyu
-git config --global user.email fuyufuyu@gmail.com
--- utils/git_ssh.sh
+++
@@ -1,7 +0,0 @@
-# gitの初期化
-git init
-
-# settingからgitのTrigger push on commitをTrueにする
-
-# ssh接続用，origin以降は適宜変更する
-git remote add origin git@github.com:fuyu-quant/Data_Science.git

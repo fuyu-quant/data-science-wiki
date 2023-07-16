392810b0ac85df044e51d4f677ddfd57e181152d
toma tanaka <ulti4929@gmail.com>
2023-07-16 13:24:58 +0000

    update

--- src/main.py
+++ src/main.py
@@ -2,11 +2,18 @@ import os
 import logging
 import subprocess
 import boto3
+from dotenv import load_dotenv
 
-s3 = boto3.client('s3')
+load_dotenv('../.env')
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    )
 
 home_path = os.environ['HOME']
-main_path = f'{home_path}/vscode/data-science-wiki/'
+main_path = f'{home_path}/data-science-wiki/'
 
 
 logging.basicConfig(
@@ -18,8 +25,8 @@ logging.basicConfig(
 
 
 def update_check():
-    result = subprocess.run(['ls'], capture_output=True)
-    #result = subprocess.run(['git', 'diff', '--name-only', 'HEAD^'], capture_output=True, text=True)
+    #result = subprocess.run(['ls'], capture_output=True)
+    result = subprocess.run(['git', 'diff', '--name-only', 'HEAD^'], capture_output=True, text=True)
 
     result = result.stdout.strip().decode('utf-8')
     result = str(result).split("\n")
@@ -30,23 +37,29 @@ def update_check():
 
 def ipynb_to_html(ipynb_list):
     html_list = []
-    for i in ipynb_list:
-        if 'ipynb' in i:
-            ipynb_path = f'main_path + {i}'
-            subprocess.run(['jupyter','nbconvert','--to','html',ipynb_path], capture_output=True)
+    for ipynb_path in ipynb_list:
+        if 'ipynb' in ipynb_path:
+            file_path = main_path + ipynb_path
+            subprocess.run(['jupyter','nbconvert','--to','html',file_path], capture_output=True)
             html_path = ipynb_path.replace('ipynb', 'html')
             html_list.append(html_path)
     return html_list
 
 
 def s3_uploader(html_list):
-    for j in html_list:
-        s3.upload_file(file_path, bucket_name, 'file.txt')
-        j
+    for html_path in html_list:
+        bucket_name = 'ds-wiki-html'
+        local_path = main_path + html_path
+        s3_path = html_path
+        s3.upload_file(local_path, bucket_name, s3_path)
+        if os.path.isfile(local_path):
+            os.remove(local_path)
     return
 
 
 if __name__ == "__main__":
-    ipynb_list = update_check()
+    #ipynb_list = update_check()
 
+    ipynb_list = ['tabledata/clustering/t-means.ipynb']
     html_list = ipynb_to_html(ipynb_list)
+    s3_uploader(html_list)

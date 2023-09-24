eeed3ae8c9e597df384281785b1f2e20e25a6348
toma tanaka <ulti4929@gmail.com>
2023-09-25 05:35:51 +0900

    update

--- tabledata/data_analysis_tools/pandas_profiling.py
+++
@@ -1,7 +0,0 @@
-! pip install -U pandas_profiling
-
-import pandas as pd
-import pandas_profiling as pdp
-
-df = pd.read_csv("https://raw.githubusercontent.com/vertica/Machine-Learning-Examples/master/data/titanic_training.csv")
-pdp.ProfileReport(df)
